f1f6efe25d3ed250a88171f3cb9991cf4dabdb11
pedroUEPB <50468850+pedroUEPB@users.noreply.github.com>
2019-09-13 12:47:47 -0300

    draw

--- ComputacaoGrafica/src/Opr/PlanoCartesiano.java
+++ ComputacaoGrafica/src/Opr/PlanoCartesiano.java
@@ -7,8 +7,12 @@ package Opr;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.util.List;
 import javax.swing.JPanel;
 
 /**
@@ -107,6 +111,71 @@ public class PlanoCartesiano extends JPanel{
             instance.getGraphics().clearRect(0, 0, this.getLargura(), this.getAltura());
             this.paint(this.getGraphics()); // Desenha o plano cartesiano novamente
         }
+    }
+
+    /**
+     * Desenha o pixel no plano cartesiano de acordo com os parametros. OBS. Os
+     * pontos não são normalizados.
+     *
+     * @param x - Coordenada x
+     * @param y - Coordenada y
+     * @param color - Cor do pixel
+     */
+    public void drawPixel(int x, int y, Color color) {
+        // Pega instância do graphics para desenhar no plano cartesiano
+        Graphics g = super.getGraphics();
+        g.setColor(color);
+        g.fillRect(x, y, 1, 1);
+
+//        bufferedImage = new BufferedImage(getLargura(),getAltura(),BufferedImage.TYPE_INT_RGB);
+//        Graphics g = bufferedImage.getGraphics();
+//        bufferedImage.setRGB(x, y, Color.LIGHT_GRAY.getRGB());
+//        g.drawImage(bufferedImage, 0, 0, null);
+    }
+
+    /**
+     * Desenha o pixel no plano cartesiano de acordo com os parametros. OBS. Os
+     * pontos são normalizados!
+     *
+     * @param x - Coordenada x
+     * @param y - Coordenada y
+     * @param color - Cor do pixel
+     */
+    public void drawPixel(double x, double y, Color color) {
+        // Normalizando os pontos
+        x = (x + PlanoCartesiano.getInstance().getValorCentroX());
+        y = (PlanoCartesiano.getInstance().getValorCentroY() - y);
+
+        drawPixel(Math.round((float) x), Math.round((float) y), color);
+    }
+
+    /**
+     * Desenha objeto 2D no plano cartesiano.
+     *
+     * @param color
+     * @param matrizObjeto
+     */
+    public void drawObjeto2D(double[][] matrizObjeto, Color color) {
+        redesenha();
+
+        // Pega instância do graphics para desenhar no plano cartesiano
+        Graphics g = this.getGraphics();
+        g.setColor(color);
+
+        g.drawLine(this.getValorCentroX() + (int) matrizObjeto[0][0], this.getValorCentroY() - (int) matrizObjeto[1][0], this.getValorCentroX() + (int) matrizObjeto[0][1], this.getValorCentroY() - (int) matrizObjeto[1][1]);
+        g.drawLine(this.getValorCentroX() + (int) matrizObjeto[0][1], this.getValorCentroY() - (int) matrizObjeto[1][1], this.getValorCentroX() + (int) matrizObjeto[0][2], this.getValorCentroY() - (int) matrizObjeto[1][2]);
+        g.drawLine(this.getValorCentroX() + (int) matrizObjeto[0][2], this.getValorCentroY() - (int) matrizObjeto[1][2], this.getValorCentroX() + (int) matrizObjeto[0][3], this.getValorCentroY() - (int) matrizObjeto[1][3]);
+        g.drawLine(this.getValorCentroX() + (int) matrizObjeto[0][3], this.getValorCentroY() - (int) matrizObjeto[1][3], this.getValorCentroX() + (int) matrizObjeto[0][0], this.getValorCentroY() - (int) matrizObjeto[1][0]);
+    }
+
+    /**
+     * Retorna o valor em RGB de acordo com o valor do pixel
+     *
+     * @param corRGB
+     * @return
+     */
+    public int getCorPixel(int corRGB) {
+        return new Color(corRGB, corRGB, corRGB).getRGB();
     }
     
 }
